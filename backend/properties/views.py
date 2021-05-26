6d930014eabef4a31725dcf13f989fe88a5d638a
Mariano Otero <mariano.otero@moni.com.ar>
2021-05-26 20:04:30 -0300

    use get method

--- backend/properties/views.py
+++ backend/properties/views.py
@@ -6,7 +6,7 @@ from properties.serializers import PropertiesSerializer
 __all__ = ['PropertiesViewSet']
 
 
-# class PropertiesViewSet(ReadOnlyModelViewSet):
 class PropertiesViewSet(ModelViewSet):
     serializer_class = PropertiesSerializer
     queryset = Property.objects.all()
+    http_method_names = ['get']
