c88e5ceb4bed0d230086d6f2b6437a1a81a5d1a6
Dario <darodangelo@gmail.com>
2021-06-27 18:38:08 -0300

    fix about component

--- frontend_vue2/src/components/home/About.vue
+++ frontend_vue2/src/components/home/About.vue
@@ -4,31 +4,64 @@
       <v-col>
         <v-card
         width="100%"
-        height="300"
+        height="auto"
         color="#BAA460"
         elevation="8"
         >
-        <v-card-title class="text-h4 text--primary">
-          Somos VIDALLED CAMARA
-        </v-card-title>
         <v-card-subtitle class="text-h6">
           Una propuesta de vanguardia para el rubro inmobiliario.
         </v-card-subtitle>
         <v-card-text>
-          <p>
             Nuestros valores transmiten responsabilidad, seriedad y confianza, como así también entusiasmo y juventud.
             Creemos en el uso de la tecnología y métodos digitales, para garantizar así un trabajo especializado y de la misma manera contribuir con el medio ambiente.
             Nuestro objetivo es crecer junto a los clientes que nos elijan, generar lazos de confianza y darle una buena imagen a la actividad.
-          </p>
+            <v-divider></v-divider>
         </v-card-text>
+
+        <template>
+          <v-row>
+            <v-col cols="12" sm="4">
+              <v-img
+              :src="`https://picsum.photos/500/300?image=35`"
+              :lazy-src="`https://picsum.photos/10/6?image=15`"
+              max-height="200"
+              aspect-ratio="1"
+              class="grey lighten-2"
+              ></v-img>
+            </v-col>
+            <v-col cols="12" sm="4">
+              <video-background
+                :src="require(`@/assets/presentacion.mp4`)"
+                style=" height: 200px;"
+              >
+                <h1 style="color: white;"></h1>
+              </video-background>
+            </v-col>
+            <v-col cols="12" sm="4">
+              <v-img
+              :src="`https://picsum.photos/500/300?image=59`"
+              :lazy-src="`https://picsum.photos/10/6?image=11`"
+              max-height="200"
+              aspect-ratio="1"
+              class="grey lighten-2"
+              ></v-img>
+            </v-col>
+          </v-row>
+        </template>
       </v-card>
     </v-col>
   </v-row>
 </v-container>
 </template>
+
+
 <script>
+import VideoBackground from 'vue-responsive-video-background-player'
 export default {
   name: "",
+  components: {
+    VideoBackground
+  },
   data: () => ({
 
   })
--- frontend_vue2/src/views/Home.vue
+++ frontend_vue2/src/views/Home.vue
@@ -1,49 +1,15 @@
 <template>
     <v-container>
-      <!-- servicios -->
       <SalesAndRent />
       <Services2 />
       <About />
-      <!-- imagenes chetas -->
-      <v-container fluid>
-        <v-row>
-          <v-col>
-            <v-img
-              :src="`https://picsum.photos/500/300?image=20`"
-              :lazy-src="`https://picsum.photos/10/6?image=25`"
-              aspect-ratio="1"
-              class="grey lighten-2"
-            ></v-img>
-          </v-col>
-
-          <v-col>
-            <video-background
-              :src="require(`@/assets/presentacion.mp4`)"
-              style="max-height: 360px; height: 100vh;"
-            >
-              <h1 style="color: white;">Hello welcome!</h1>
-            </video-background>
-          </v-col>
-
-          <v-col>
-            <v-img
-              :src="`https://picsum.photos/500/300?image=10`"
-              :lazy-src="`https://picsum.photos/10/6?image=15`"
-              aspect-ratio="1"
-              class="grey lighten-2"
-            ></v-img>
-          </v-col>
-        </v-row>
-      </v-container>
     </v-container>
-
 </template>
 
 <script>
 import SalesAndRent from "@/components/home/SalesAndRent.vue"
 import Services2 from "@/components/home/Services2.vue"
 import About from "@/components/home/About.vue"
-import VideoBackground from 'vue-responsive-video-background-player'
 
 
 export default {
@@ -52,8 +18,6 @@ export default {
     SalesAndRent,
     Services2,
     About,
-    VideoBackground
-
   },
   data() {
     return {
