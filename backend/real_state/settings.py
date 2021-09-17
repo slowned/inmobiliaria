16c5a4d06d2cbf7257a0b9902cbf8044987bd1be
slowned <mariano.m3n@gmail.com>
2021-09-16 21:12:50 -0300

    adds more cors variables

--- backend/real_state/settings.py
+++ backend/real_state/settings.py
@@ -133,6 +133,12 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8080",
+    "http://127.0.0.1:8080",
+]
 
 CORS_ORIGIN_ALLOW_ALL = True
 # CORS_ORIGIN_WHITELIST = (
