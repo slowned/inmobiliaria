0eec0572ce2689973ea806b586f77538b973ed5f
mariano <mllambay@hindsightsolutions.net>
2023-03-21 21:11:26 -0300

    create component to modal response WIP

---
+++ frontend_vue2/src/components/property_create/ModalCreated.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="text-center">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ModalCreated',
+  props: {
+    showModal: Boolean,
+  },
+  data() {
+    return {
+      localShowModal: this.showModal
+    }
+  },
+  watch: {
+    localShowModal(newValue) {
+      this.$emit('update:showModal', newValue)
+    },
+  },
+}
+</script>
